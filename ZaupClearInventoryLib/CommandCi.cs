bac51abe2883407f671a6e754fc69a1e4f63266f
Zamirathe <zamirathe@centurylink.net>
2015-07-31 16:26:29 -0600

    Initial Release

---
+++ ZaupClearInventoryLib/CommandCi.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Rocket.Core;
+using Rocket.Core.Permissions;
+using Rocket.Unturned;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Commands;
+using Rocket.Unturned.Player;
+using Rocket.API;
+using SDG.Unturned;
+
+namespace ZaupClearInventoryLib
+{
+    public class CommandCi : IRocketCommand
+    {
+        public bool AllowFromConsole
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return "ci";
+            }
+        }
+        public string Help
+        {
+            get
+            {
+                return "/ci [name/self] [true] will clears your or someone else's inventory and true will remove clothes too.";
+            }
+        }
+        public string Syntax
+        {
+            get
+            {
+                return "/ci [name/self] [true]";
+            }
+        }
+        public List<string> Aliases
+        {
+            get 
+            { 
+                return new List<string>()
+                {
+                    "clearinventory",
+                    "cleari"
+                }; 
+            }
+        }
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>()
+                {
+                    "ci.other",
+                    "ci"
+                };
+            }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] msg)
+        {
+            UnturnedPlayer playerid = (UnturnedPlayer)caller;
+            if (msg.Length > 2)
+            {
+                UnturnedChat.Say(playerid, "Invalid use of ci.");
+                return;
+            }
+            UnturnedPlayer player = playerid;
+            if (msg.Length >= 1)
+            {
+                if (msg[0].ToLower() != "self")
+                {
+                    bool hasp = R.Permissions.HasPermission(playerid, "ci.other");
+                    if (!hasp && !playerid.IsAdmin)
+                    {
+                        UnturnedChat.Say(playerid, "You do not have permission to clear someone else's inventory.");
+                        return;
+                    }
+                    player = UnturnedPlayer.FromName(msg[0]);
+                }
+            }
+            bool done = ZaupClearInventoryLib.Instance.ClearInv(player);
+            if (msg.Length == 2)
+            {
+                if (msg[1].ToLower() != "true")
+                {
+                    UnturnedChat.Say(playerid, "Invalid use of the command.  /ci [name/self] [true]");
+                    return;
+                }
+                done = ZaupClearInventoryLib.Instance.ClearClothes(player);
+            }
+            if (!done)
+            {
+                UnturnedChat.Say(playerid, "There was an error.  Please look at your console error log.");
+            }
+            else
+            {
+                UnturnedChat.Say(playerid, player.CharacterName + "'s inventory has been cleared.");
+            }
+        }
+    }
+}
---
+++ ZaupClearInventoryLib/ZaupClearInventoryLib.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Rocket.API;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using Rocket.Unturned.Plugins;
+using SDG.Unturned;
+
+namespace ZaupClearInventoryLib
+{
+    public class ZaupClearInventoryLib : RocketPlugin
+    {
+        public static ZaupClearInventoryLib Instance;
+
+        protected override void Load()
+        {
+            ZaupClearInventoryLib.Instance = this;
+        }
+
+        public bool ClearInv(UnturnedPlayer player)
+        {
+            bool returnv = false;
+            try
+            {
+                player.Player.equipment.dequip();
+                for (byte p = 0; p < PlayerInventory.PAGES; p++)
+                {
+                    byte itemc = player.Player.inventory.getItemCount(p);
+                    if (itemc > 0)
+                    {
+                        for (byte p1 = 0; p1 < itemc; p1++)
+                        {
+                            player.Player.inventory.removeItem(p, 0);
+                        }
+                    }
+                }
+                player.Player.SteamChannel.send("tellSlot", ESteamCall.ALL, ESteamPacket.UPDATE_TCP_BUFFER, new object[]
+		        {
+			        (byte)0,
+			        (byte)0,
+			        new byte[0]
+		        });
+                player.Player.SteamChannel.send("tellSlot", ESteamCall.ALL, ESteamPacket.UPDATE_TCP_BUFFER, new object[]
+		        {
+			        (byte)1,
+			        (byte)0,
+			        new byte[0]
+		        });
+                returnv = true;
+            }
+            catch (Exception e)
+            {
+                Logger.Log("There was an error clearing " + player.CharacterName + "'s inventory.  Here is the error.");
+                Console.Write(e);
+            }
+            return returnv;
+        }
+
+        public bool ClearClothes(UnturnedPlayer player)
+        {
+            bool returnv = false;
+            try
+            {
+                player.Player.Clothing.askWearBackpack(0, 0, new byte[0]);
+                for (byte p2 = 0; p2 < player.Player.Inventory.getItemCount(2); p2++)
+                {
+                    player.Player.Inventory.removeItem(2, 0);
+                }
+                player.Player.Clothing.askWearGlasses(0, 0, new byte[0]);
+                for (byte p2 = 0; p2 < player.Player.Inventory.getItemCount(2); p2++)
+                {
+                    player.Player.Inventory.removeItem(2, 0);
+                }
+                player.Player.Clothing.askWearHat(0, 0, new byte[0]);
+                for (byte p2 = 0; p2 < player.Player.Inventory.getItemCount(2); p2++)
+                {
+                    player.Player.Inventory.removeItem(2, 0);
+                }
+                player.Player.Clothing.askWearMask(0, 0, new byte[0]);
+                for (byte p2 = 0; p2 < player.Player.Inventory.getItemCount(2); p2++)
+                {
+                    player.Player.Inventory.removeItem(2, 0);
+                }
+                player.Player.Clothing.askWearPants(0, 0, new byte[0]);
+                for (byte p2 = 0; p2 < player.Player.Inventory.getItemCount(2); p2++)
+                {
+                    player.Player.Inventory.removeItem(2, 0);
+                }
+                player.Player.Clothing.askWearShirt(0, 0, new byte[0]);
+                for (byte p2 = 0; p2 < player.Player.Inventory.getItemCount(2); p2++)
+                {
+                    player.Player.Inventory.removeItem(2, 0);
+                }
+                player.Player.Clothing.askWearVest(0, 0, new byte[0]);
+                for (byte p2 = 0; p2 < player.Player.Inventory.getItemCount(2); p2++)
+                {
+                    player.Player.Inventory.removeItem(2, 0);
+                }
+                player.Player.SteamChannel.send("tellClothing", ESteamCall.ALL, ESteamPacket.UPDATE_UDP_BUFFER, new object[]
+                {
+                    (byte)0,
+                    (byte)0,
+                    (byte)0,
+                    (byte)0,
+                    (byte)0,
+                    (byte)0,
+                    (byte)0,
+                    (byte)0,
+                    (byte)0,
+                    (byte)0,
+                    (byte)0,
+                    (byte)0,
+                    (byte)0,
+                    (byte)0
+                });
+                returnv = true;
+            }
+            catch (Exception e)
+            {
+                Logger.Log("There was an error clearing " + player.CharacterName + "'s inventory.  Here is the error.");
+                Console.Write(e);
+            }
+            return returnv;
+        }
+    }
+}

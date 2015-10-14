e118f9c022959f47f586507458faaebc1b310919
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-10-14 19:46:50 +0200

    For free...

---
+++ ZaupClearInventoryLibConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.API;
+
+namespace ZaupClearInventoryLib
+{
+    public class ZaupClearInventoryLibConfiguration : IRocketPluginConfiguration
+    {
+        public bool DeleteInventoryOnDeath;
+        public void LoadDefaults()
+        {
+            DeleteInventoryOnDeath = false;
+        }
+    }
+}
